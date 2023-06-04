9cd82d11f4321eb569061908f8b95d3335f0e140
Wanqin liang <liangwan@sheridancollege.ca>
2023-06-04 09:36:17 -0400

    hollo world thurday

---
+++ src/Students.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author tingman
+ */
+public class Students {
+    private String name;
+    private String studentId;
+    private String address;
+    public Students (String giveName){
+       name=giveName;   
+    }
+    private void String getStudentId(){
+        this.studentId=studentId;
+    }
+  
+    
+    
+}
